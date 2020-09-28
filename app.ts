32965b44ef60f1b8669b2c138b35aa810276e793
Ruma Das <rumaadl16@gmail.com>
2020-09-28 11:25:18 +0930

    adding type in app.ts file

--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log("Your codes goes here!");
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //     throw new Error("INcorrect input");
+  //   }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printRessult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printRessult, resultPhrase);

7106cba243882e8dbfee3e58950c8dcb9588ef8a
Ruma Das <rumaadl16@gmail.com>
2020-10-06 16:15:17 +1030

    adding component folder and API and utilits tsx file

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-function App() {
-  return <div className="App">Quize</div>;
-}
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quize</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions......</p>
+    </div>
+  );
+};
 
 export default App;
